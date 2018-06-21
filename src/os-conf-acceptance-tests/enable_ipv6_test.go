8ebca201c86fd852dfe860ed3d3f652f96702917
Luan Santos <lsantos@pivotal.io>
2018-06-21 10:14:47 -0700

    Test sysctl ipv6 values AFTER applying conf
    [#158400410](https://www.pivotaltracker.com/story/show/158400410)
    
    Co-authored-by: Michael Xu <mxu@pivotal.io>

--- src/os-conf-acceptance-tests/enable_ipv6_test.go
+++ src/os-conf-acceptance-tests/enable_ipv6_test.go
@@ -11,7 +11,12 @@ import (
 
 var _ = Describe("Enable IPV6", func() {
 	It("enables ipv6 on a VM", func() {
-		session := boshSSH("os-conf/0", "sudo cat /etc/sysctl.d/61-bosh-enable_ipv6-release.conf")
+		session := boshSSH("os-conf/0", "sudo sysctl --system")
+		Eventually(session, 30*time.Second).Should(gbytes.Say("\\* Applying /etc/sysctl\\.d/10-ipv6-privacy\\.conf \\.\\.\\."))
+		Eventually(session, 30*time.Second).Should(gbytes.Say("\\* Applying /etc/sysctl\\.d/61-bosh-enable_ipv6-release\\.conf \\.\\.\\."))
+		Eventually(session, 30*time.Second).Should(gexec.Exit(0))
+
+		session = boshSSH("os-conf/0", "sudo sysctl -a")
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv6.conf.all.use_tempaddr = 0"))
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv6.conf.default.use_tempaddr = 0"))
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv6.conf.all.accept_ra=1"))
@@ -21,11 +26,6 @@ var _ = Describe("Enable IPV6", func() {
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv6.conf.default.accept_redirects=1"))
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv6.conf.all.accept_redirects=1"))
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv6.route.flush=0"))
-		Eventually(session, 30*time.Second).Should(gexec.Exit(0))
-
-		session = boshSSH("os-conf/0", "sudo sysctl --system")
-		Eventually(session, 30*time.Second).Should(gbytes.Say("\\* Applying /etc/sysctl\\.d/10-ipv6-privacy\\.conf \\.\\.\\."))
-		Eventually(session, 30*time.Second).Should(gbytes.Say("\\* Applying /etc/sysctl\\.d/61-bosh-enable_ipv6-release\\.conf \\.\\.\\."))
 		Eventually(session, 30*time.Second).Should(gexec.Exit(0))
 	})
 })
