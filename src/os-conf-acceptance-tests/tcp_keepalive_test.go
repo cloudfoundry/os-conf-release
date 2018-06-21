97770c009fbdbc8b10db7226cd723d8c7f3bbb26
Michael Xu <mxu@pivotal.io>
2018-06-21 12:04:30 -0700

    Add -w to sysctl set calls
    [#158400410](https://www.pivotaltracker.com/story/show/158400410)
    
    Co-authored-by: Luan Santos <lsantos@pivotal.io>

--- src/os-conf-acceptance-tests/tcp_keepalive_test.go
+++ src/os-conf-acceptance-tests/tcp_keepalive_test.go
@@ -17,15 +17,9 @@ var _ = Describe("TCPKeepAlive", func() {
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv4.tcp_keepalive_probes=12"))
 		Eventually(session, 30*time.Second).Should(gexec.Exit(0))
 
-		session = boshSSH("os-conf/0", "sudo sysctl net.ipv4.tcp_keepalive_time")
+		session = boshSSH("os-conf/0", "sudo sysctl -a")
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv4.tcp_keepalive_time = 10"))
-		Eventually(session, 30*time.Second).Should(gexec.Exit(0))
-
-		session = boshSSH("os-conf/0", "sudo sysctl net.ipv4.tcp_keepalive_intvl")
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv4.tcp_keepalive_intvl = 11"))
-		Eventually(session, 30*time.Second).Should(gexec.Exit(0))
-
-		session = boshSSH("os-conf/0", "sudo sysctl net.ipv4.tcp_keepalive_probes")
 		Eventually(session, 30*time.Second).Should(gbytes.Say("net.ipv4.tcp_keepalive_probes = 12"))
 		Eventually(session, 30*time.Second).Should(gexec.Exit(0))
 	})
