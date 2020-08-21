8498f79145e925a1330d3a5fe67e4e9daa67cf82
Diego Lemos <diego@diegolemos.net>
2020-08-21 19:17:42 +0100

    Fix shellcheck warnings

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,9 +2,10 @@
 
 set -exu
 
-dir=$(dirname $0)
+dir="$(dirname "$0")"
 
-fly -t ${CONCOURSE_TARGET:-production} \
+fly -t "${CONCOURSE_TARGET:-production}" \
   sp -p os-conf-release \
-  -c $dir/pipeline.yml \
+  -c "$dir/pipeline.yml" \
   -l <(lpass show --notes 'os-conf-release pipeline vars') \
+
