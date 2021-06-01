86ed096a2d761b131f1827b6a18a98ebad76f91e
Joseph Palermo <jpalermo@vmware.com>
2021-06-01 14:55:01 -0700

    Update to run against pre-created BOSH Director
    [#178318006] cloudfoundry/os-conf-release #61: Run acceptance tests on bionic stemcell
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

--- ci/tasks/run-acceptance-tests.sh
+++ ci/tasks/run-acceptance-tests.sh
@@ -8,25 +8,18 @@ export PATH="${GOPATH}/bin:$PATH"
 apt-get update
 apt-get install -y netcat-openbsd
 
-BBL_VERSION=6.7.10
-BBL_SHA=53bd673f2967b5b82a3db4862c823230f55fe7d2f57bcec8241318e4ba9d8e60
-curl -fSL https://github.com/cloudfoundry/bosh-bootloader/releases/download/v${BBL_VERSION}/bbl-v${BBL_VERSION}_linux_x86-64 -o /usr/bin/bbl \
-  && echo "$BBL_SHA  /usr/bin/bbl" | shasum -c - \
-  && chmod +x /usr/bin/bbl
+jumpbox_url=${BOSH_JUMPBOX_URL:-${BOSH_JUMPBOX_IP}:22}
+jumpbox_private_key_path=$(mktemp)
+chmod 600 ${jumpbox_private_key_path}
+echo "${BOSH_JUMPBOX_PRIVATE_KEY}" > ${jumpbox_private_key_path}
 
-eval "$(bbl --state-dir "${PWD}/bbl-state" print-env)"
-
-BOSH_VERSION=3.0.1
-BOSH_SHA=ccc893bab8b219e9e4a628ed044ebca6c6de9ca0
-curl -fSL https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-${BOSH_VERSION}-linux-amd64 -o /usr/bin/bosh \
-  && echo "$BOSH_SHA  /usr/bin/bosh" | shasum -c - \
-  && chmod +x /usr/bin/bosh
+export BOSH_ALL_PROXY=ssh+socks5://${BOSH_JUMPBOX_USER}@${jumpbox_url}?private-key=${jumpbox_private_key_path}
 
 bosh upload-stemcell ${PWD}/stemcell/*.tgz
 bosh create-release --dir ${PWD}/os-conf-release --timestamp-version --tarball=release.tgz
 bosh upload-release release.tgz
 
-export BOSH_BINARY_PATH=/usr/bin/bosh
+export BOSH_BINARY_PATH=$(which bosh)
 
 pushd "${PWD}/os-conf-release/src/os-conf-acceptance-tests"
   go run github.com/onsi/ginkgo/ginkgo -v
